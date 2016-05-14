82aea5668be794e9f141915e9cb59fa629ee6981
dhesusan <dhesusan@yahoo.co.jp>
2016-05-14 22:58:21 +0900

    UI部分仮実装

--- app/src/main/java/wcdi/spajam2016/QuestionsFragment.java
+++ app/src/main/java/wcdi/spajam2016/QuestionsFragment.java
@@ -64,7 +64,7 @@ public class QuestionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_questions, container, false);
+        return inflater.inflate(R.layout.fragment_question_list, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
